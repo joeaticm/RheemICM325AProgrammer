cc6551f6afb7cbe0cbe54717625167b6596ad175
Joseph Gerber <jgerber@icmcontrols.com>
2024-12-12 14:07:34 -0500

    Implemented UI.

---
+++ UI/ICM325A.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Management;
+using System.Printing.IndexedProperties;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace RheemICM325AProgrammer
+{
+    public class ICM325A
+    {
+        // False - Temperature True - Pressure
+        public bool ProbeType { get; set; }
+        // 50-500
+        public ushort SetPoint { get; set; }
+        // 1-50
+        public byte HardStart { get; set; }
+        // 17-48
+        public byte MinimumOutputVoltage { get; set; }
+
+        const byte FUNCTION_ID = 201;
+
+        public byte[] GetProgram()
+        {
+            byte[] program = new byte[16];
+
+            program[1] = FUNCTION_ID;
+            program[2] = Convert.ToByte(ProbeType);
+            program[3] = (byte)(SetPoint >> 8);
+            program[4] = (byte)(SetPoint);
+            program[5] = HardStart;
+            program[6] = MinimumOutputVoltage;
+
+
+            byte checksum = CalculateTwosComplementChecksum(program);
+            program[0] = checksum;
+
+            return program;
+        }
+        public bool Program()
+        {
+            string port;
+
+            using (var searcher = new ManagementObjectSearcher("SELECT * FROM Win32_PnPEntity WHERE Caption like '%(COM%'"))
+            {
+                var portNames = SerialPort.GetPortNames();
+                var ports = searcher.Get().Cast<ManagementBaseObject>().ToList().Select(p => p["Caption"].ToString());
+                port = portNames.First(port => ports.First(s => s != null && s.Contains(port)).Contains("STMicroelectronics STLink Virtual COM Port"));
+            }
+
+            byte[] program = GetProgram();
+
+            SerialPort serialPort = new SerialPort
+            {
+                PortName = port,
+                BaudRate = 19200,
+            };
+            serialPort.Open();
+            serialPort.Write("P");
+            for (int i = 0; i < 16; i++)
+            {
+                serialPort.Write(program, i, 1);
+            }
+            string result = serialPort.ReadLine();
+            serialPort.Close();
+            if (result == "PASS")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        static byte CalculateTwosComplementChecksum(byte[] data)
+        {
+            int sum = 0;
+
+            // Calculate the sum of all bytes
+            foreach (var b in data)
+            {
+                sum += b;
+            }
+
+            // Calculate the 2's complement
+            byte checksum = (byte)((~sum + 1) & 0xFF);
+
+            return checksum;
+        }
+    }
+}
---
+++ UI/MainWindow.xaml.cs
@@ -0,0 +1,119 @@
+﻿using System.Diagnostics;
+using System.IO.Ports;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RheemICM325AProgrammer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ICM325A? icm;
+        bool programmer_running = false;
+
+        const string messageWaiting = "Scan a barcode to select configuration.";
+        const string messageReady = "Press \"Program\" to write configuration to ICM325A.";
+
+        const string messageIdle = "IDLE";
+        const string messageRunning = "RUNNING";
+        const string messagePass = "PASS";
+        const string messageFail = "FAIL";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            RemoveICM();
+        }
+
+        private void RemoveICM()
+        {
+            icm = null;
+            Status.Text = messageWaiting;
+            Program.Visibility = Visibility.Hidden;
+            ProgramStatus.Visibility = Visibility.Hidden;
+
+            ProbeType.Text = "";
+            SetPoint.Text = "";
+            HardStart.Text = "";
+            MinimumOutputVoltage.Text = "";
+        }
+
+        private void AddICM(ICM325A icm)
+        {
+            this.icm = icm;
+            Status.Text = messageReady;
+            Program.Visibility = Visibility.Visible;
+            ProgramStatus.Text = messageIdle;
+            ProgramStatus.Visibility = Visibility.Visible;
+
+            if (icm.ProbeType)
+            {
+                ProbeType.Text = "Pressure";
+            }
+            else
+            {
+                ProbeType.Text = "Temperature";
+            }
+            
+            SetPoint.Text = $"{icm.SetPoint} psi";
+            HardStart.Text = $"{icm.HardStart / 10.0:0.0} s";
+            MinimumOutputVoltage.Text = $"{icm.MinimumOutputVoltage} %";
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (programmer_running)
+            {
+                return;
+            }
+            programmer_running = true;
+            if (icm != null)
+            {
+
+                ProgramStatus.Text = messageRunning;
+                bool result = await Task.Run(icm.Program);
+                if (result)
+                {
+                    ProgramStatus.Text = messagePass;
+                }
+                else
+                {
+                    ProgramStatus.Text = messageFail;
+                }
+                programmer_running = false;
+            }
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.A)
+            {
+                ICM325A icm = new ICM325A();
+                icm.ProbeType = true;
+                icm.SetPoint = 55;
+                icm.HardStart = 30;
+                icm.MinimumOutputVoltage = 40;
+                AddICM(icm);
+            }
+            else if (e.Key == Key.B)
+            {
+                ICM325A icm = new ICM325A();
+                icm.ProbeType = false;
+                icm.SetPoint = 400;
+                icm.HardStart = 40;
+                icm.MinimumOutputVoltage = 17;
+                AddICM(icm);
+            }
+        }
+    }
+}
